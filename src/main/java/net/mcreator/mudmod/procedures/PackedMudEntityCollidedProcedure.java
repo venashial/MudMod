9f9c26b3d9095021b668cd51be49ec0759af01ce
venashial <44736536+venashial@users.noreply.github.com>
2020-10-04 01:04:12 -0700

    Add files via upload

--- src/main/java/net/mcreator/mudmod/procedures/PackedMudEntityCollidedProcedure.java
+++ src/main/java/net/mcreator/mudmod/procedures/PackedMudEntityCollidedProcedure.java
@@ -1,12 +1,18 @@
 package net.mcreator.mudmod.procedures;
 
 import net.minecraft.world.IWorld;
+import net.minecraft.world.GameType;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.potion.Effects;
 import net.minecraft.potion.EffectInstance;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.Entity;
+import net.minecraft.client.network.play.NetworkPlayerInfo;
+import net.minecraft.client.entity.player.ClientPlayerEntity;
+import net.minecraft.client.Minecraft;
 
 import net.mcreator.mudmod.block.PackedMudBlock;
 import net.mcreator.mudmod.MudModModElements;
@@ -41,7 +47,45 @@ public class PackedMudEntityCollidedProcedure extends MudModModElements.ModEleme
 		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
 		IWorld world = (IWorld) dependencies.get("world");
 		String EntityName = "";
-		entity.setMotionMultiplier(null, new Vec3d(0.25D, (double) 0.05F, 0.25D));
+		if (((true) == (((new Object() {
+			public boolean checkGamemode(Entity _ent) {
+				if (_ent instanceof ServerPlayerEntity) {
+					return ((ServerPlayerEntity) _ent).interactionManager.getGameType() == GameType.ADVENTURE;
+				} else if (_ent instanceof PlayerEntity && _ent.world.isRemote) {
+					NetworkPlayerInfo _npi = Minecraft.getInstance().getConnection()
+							.getPlayerInfo(((ClientPlayerEntity) _ent).getGameProfile().getId());
+					return _npi != null && _npi.getGameType() == GameType.ADVENTURE;
+				}
+				return false;
+			}
+		}.checkGamemode(entity)) || (new Object() {
+			public boolean checkGamemode(Entity _ent) {
+				if (_ent instanceof ServerPlayerEntity) {
+					return ((ServerPlayerEntity) _ent).interactionManager.getGameType() == GameType.SURVIVAL;
+				} else if (_ent instanceof PlayerEntity && _ent.world.isRemote) {
+					NetworkPlayerInfo _npi = Minecraft.getInstance().getConnection()
+							.getPlayerInfo(((ClientPlayerEntity) _ent).getGameProfile().getId());
+					return _npi != null && _npi.getGameType() == GameType.SURVIVAL;
+				}
+				return false;
+			}
+		}.checkGamemode(entity))) || (new Object() {
+			public boolean checkGamemode(Entity _ent) {
+				if (_ent instanceof ServerPlayerEntity) {
+					return ((ServerPlayerEntity) _ent).interactionManager.getGameType() == GameType.CREATIVE;
+				} else if (_ent instanceof PlayerEntity && _ent.world.isRemote) {
+					NetworkPlayerInfo _npi = Minecraft.getInstance().getConnection()
+							.getPlayerInfo(((ClientPlayerEntity) _ent).getGameProfile().getId());
+					return _npi != null && _npi.getGameType() == GameType.CREATIVE;
+				}
+				return false;
+			}
+		}.checkGamemode(entity))))) {
+			entity.setMotionMultiplier(null, new Vec3d(0.25D, (double) 0.05F, 0.25D));
+		} else {
+			if (entity instanceof LivingEntity)
+				((LivingEntity) entity).addPotionEffect(new EffectInstance(Effects.SLOWNESS, (int) 20, (int) 4, (false), (false)));
+		}
 		if (((world.getBlockState(new BlockPos((int) x, (int) ((entity.getPosY()) + 1), (int) z))).getBlock() == PackedMudBlock.block
 				.getDefaultState().getBlock())) {
 			if (entity instanceof LivingEntity)
